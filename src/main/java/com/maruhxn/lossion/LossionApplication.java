2545a2caa1c81e41acef076a4b23359563b58d7e
maruhxn_ <maruhan1016@gmail.com>
2024-01-03 10:32:41 +0900

    Initial Commit :)

---
+++ src/main/java/com/maruhxn/lossion/LossionApplication.java
@@ -0,0 +1,13 @@
+package com.maruhxn.lossion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LossionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LossionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maruhxn/lossion/LossionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maruhxn.lossion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LossionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
