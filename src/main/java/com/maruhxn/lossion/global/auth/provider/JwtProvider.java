9d83bd6302b5376484874aeae74197f1b9cbfa00
maruhxn_ <maruhan1016@gmail.com>
2024-01-04 16:34:10 +0900

    Feat: JwtProvider
    Related to: #7

---
+++ src/main/java/com/maruhxn/lossion/global/auth/provider/JwtProvider.java
@@ -0,0 +1,49 @@
+package com.maruhxn.lossion.global.auth.provider;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+    @Value("${jwt.secret-key}")
+    private static String SECRET_KEY;
+
+    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+
+    public String create(String email) {
+        Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
+
+        return Jwts.builder()
+                .signWith(key, SignatureAlgorithm.HS256)
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(expiredDate)
+                .compact();
+    }
+
+    public String validate(String jwt) {
+        Claims claims = null;
+
+        try {
+            claims = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(jwt)
+                    .getBody();
+        } catch (Exception e) {
+            return null;
+        }
+
+        return claims.getSubject();
+    }
+}
