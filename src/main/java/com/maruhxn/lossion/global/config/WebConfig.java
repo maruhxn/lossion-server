da4c83637204a589f4120dcab40e61c6dc34e7f0
maruhxn_ <maruhan1016@gmail.com>
2024-01-09 23:26:43 +0900

    Feat: TopicAuthorCheckInterceptor
    Related to: #11

--- src/main/java/com/maruhxn/lossion/global/config/WebConfig.java
+++ src/main/java/com/maruhxn/lossion/global/config/WebConfig.java
@@ -1,13 +1,33 @@
 package com.maruhxn.lossion.global.config;
 
+import com.maruhxn.lossion.domain.topic.dao.TopicRepository;
+import com.maruhxn.lossion.global.interceptor.TopicAuthorCheckInterceptor;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final TopicRepository topicRepository;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(topicAuthorCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/api/topics/{topicId}/**");
+    }
+
+    @Bean
+    public TopicAuthorCheckInterceptor topicAuthorCheckInterceptor() {
+        return new TopicAuthorCheckInterceptor(topicRepository);
+    }
+
     @Bean
     JPAQueryFactory jpaQueryFactory(EntityManager em) {
         return new JPAQueryFactory((em));
