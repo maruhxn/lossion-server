7657a3155cb035e1ce559400ccca2906ddd93fa1
maruhxn_ <maruhan1016@gmail.com>
2024-01-16 00:12:55 +0900

    Feat: CommentFavoriteRepository, TopicFavoriteRepository
    Related to: #15

--- src/main/java/com/maruhxn/lossion/domain/favorite/dao/TopicFavoriteRepository.java
+++ src/main/java/com/maruhxn/lossion/domain/favorite/dao/TopicFavoriteRepository.java
@@ -3,5 +3,11 @@ package com.maruhxn.lossion.domain.favorite.dao;
 import com.maruhxn.lossion.domain.favorite.domain.TopicFavorite;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TopicFavoriteRepository extends JpaRepository<TopicFavorite, Long> {
+
+    Optional<TopicFavorite> findByTopic_IdAndMember_Id(Long topicId, Long memberId);
+
+    boolean existsByTopic_IdAndMember_Id(Long topicId, Long memberId);
 }
